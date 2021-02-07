248d14d3aa96dece91949715e00bf31eadb446eb
ddlss <925353524@qq.com>
2021-02-07 10:58:40 +0800

    '首页轮播图'

--- src/components/content/mainTabBar/MainTabBar.vue
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -1,18 +1,18 @@
 <template>
   <tab-bar>
-      <tab-bar-item link='/home' activeColor="blue">
+      <tab-bar-item link='/home'>
         <i slot="item-icon" class="iconfont icon-home-filling"></i>
         <div slot="item-text">首页</div>
       </tab-bar-item>
-      <tab-bar-item link= '/category' activeColor="blue">
+      <tab-bar-item link= '/category'>
         <i slot="item-icon" class="iconfont icon-modular"></i>
         <div slot="item-text">分类</div>
       </tab-bar-item>
-      <tab-bar-item link= '/shopcart' activeColor="blue">
+      <tab-bar-item link= '/shopcart'>
         <i slot="item-icon" class="iconfont icon-gouwuche"></i>
         <div slot="item-text">购物车</div>
       </tab-bar-item>
-      <tab-bar-item link= '/profile' activeColor="blue">
+      <tab-bar-item link= '/profile'>
         <i slot="item-icon" class="iconfont icon-user-filling"></i>
         <div slot="item-text">我的</div>
       </tab-bar-item>
@@ -20,8 +20,8 @@
 </template>
 
 <script>
-import TabBar from 'components/tabbar/TabBar'
-import TabBarItem from 'components/tabbar/TabBarItem'
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
 export default {
   name: 'MainTabBar',
   components: {
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+      <div>{{item.title}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecommendView',
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .recommend {
+    display: flex;
+    text-align: center;
+    width: 100%;
+    font-size: 13px;
+    padding: 10px 0 20px;
+    border-bottom: 10px solid #eee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    height: 70px;
+    width: 70px;
+    margin-bottom: 4px;
+  }
+</style>
